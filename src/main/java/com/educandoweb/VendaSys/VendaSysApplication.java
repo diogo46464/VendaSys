69d86e61de6489d46706cf1764554302d36dc77f
Diogo Pincetta <diogohe08@gmail.com>
2026-04-02 09:34:02 -0300

    Project created

---
+++ src/main/java/com/educandoweb/VendaSys/VendaSysApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.VendaSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/VendaSys/VendaSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.VendaSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
