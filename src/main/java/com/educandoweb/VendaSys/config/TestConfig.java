9b2c65560d56e677f269a14f15094957e7462fa8
Diogo Pincetta <diogohe08@gmail.com>
2026-04-07 11:42:36 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/VendaSys/config/TestConfig.java
+++ src/main/java/com/educandoweb/VendaSys/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.VendaSys.entities.Category;
 import com.educandoweb.VendaSys.entities.Order;
 import com.educandoweb.VendaSys.entities.OrderItem;
+import com.educandoweb.VendaSys.entities.Payment;
 import com.educandoweb.VendaSys.entities.Product;
 import com.educandoweb.VendaSys.entities.User;
 import com.educandoweb.VendaSys.entities.enums.OrderStatus;
@@ -85,6 +86,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
